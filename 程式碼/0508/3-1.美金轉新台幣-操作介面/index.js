2c23f3b3fbda4f3d4ea14f355f31966033eb8f68
yisichoco <yisichoco@gmail.com>
2023-05-08 15:34:25 +0800

    A

---
+++ 程式碼/0508/3-1.美金轉新台幣-操作介面/index.js
@@ -0,0 +1,11 @@
+// 先獲取所有所需DOM元素物件
+// input
+const money = document.getElementById('money');
+// button
+const u2t = document.getElementById('u2t');
+const t2u = document.getElementById('t2u');
+// span
+const result = document.getElementById('result');
+
+// 檢查是否有獲取到元素物件
+console.log(money, u2t, t2u, result);
