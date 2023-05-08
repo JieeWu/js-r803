2ce770dbbdbf648b417f567bfdfa86462e942c18
yisichoco <yisichoco@gmail.com>
2023-05-08 16:15:38 +0800

    Update index.js

--- 程式碼/0508/4-1.BMI計算-操作介面/index.js
+++ 程式碼/0508/4-1.BMI計算-操作介面/index.js
@@ -1,3 +1,10 @@
 // 先獲取所有所需DOM元素物件
 // input
 const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const calc = document.getElementById('calc');
+const result = document.getElementById('result');
+
+
+// 測試
+console.log(height, weight, calc, result);
