90e3bc93cf39a2d6ead6e4df825d67c6512b98a7
Eddy Chang <eddy@joomla.com.tw>
2023-05-09 15:19:30 +0800

    A

--- 程式碼/0509/7-0.擲骰子-介面/index.js
+++ 程式碼/0509/7-0.擲骰子-介面/index.js
@@ -1,1 +1,14 @@
-//dom
+const start = document.getElementById('start')
+
+start.addEventListener('click',function(){
+    const rand = Math.floor(Math.random() * 6) + 1
+
+    // const diceWords = ['one', 'two']
+
+    // let diceType = ''
+
+    // switch(rand){
+    //     case 1 :
+    //         diceType='one'
+    // }
+})
