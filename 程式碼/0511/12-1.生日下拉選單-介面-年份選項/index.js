f2cb8f8a719464b96387a76fbaa5e5aa575d0dc6
yisichoco <yisichoco@gmail.com>
2023-05-11 16:02:02 +0800

    A

---
+++ 程式碼/0511/12-1.生日下拉選單-介面-年份選項/index.js
@@ -0,0 +1,24 @@
+// 獲得dom元素物件
+const yy = document.getElementById('yy');
+const mm = document.getElementById('mm');
+const dd = document.getElementById('dd');
+
+// 檢查
+console.log(yy, mm, dd);
+
+// 產生西元年的選項
+// 1950~2020
+// <option value="1950">1950</option>
+let yearOptions = '';
+
+for (let i = 1950; i < 2021; i++) {
+    yearOptions += `<option value="${i}">${i}</option>`;
+}
+
+//console.log(yearOptions);
+yy.innerHTML = yearOptions;
+
+// 產生月的選項 1~12
+
+// 產生日的選項 1~31
+
