f8ccb8108296560f562db4dff353daa42c0329ca
yisichoco <yisichoco@gmail.com>
2023-05-15 11:56:17 +0800

    Update index.js

--- 程式碼/0515/17-2.十點半遊戲-洗牌演算法/index.js
+++ 程式碼/0515/17-2.十點半遊戲-洗牌演算法/index.js
@@ -31,6 +31,8 @@ function createCard(suit, point) {
 }
 
 // 洗牌演算法
+// https://bost.ocks.org/mike/shuffle/
+// https://gaohaoyang.github.io/2016/10/16/shuffle-algorithm/
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
