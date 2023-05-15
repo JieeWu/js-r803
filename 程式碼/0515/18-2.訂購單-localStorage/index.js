5be2624f5e65573f273c8d74bada6f9955a2fe07
yisichoco <yisichoco@gmail.com>
2023-05-15 14:30:16 +0800

    A

--- 程式碼/0515/18-2.訂購單-localStorage/index.js
+++ 程式碼/0515/18-2.訂購單-localStorage/index.js
@@ -57,8 +57,30 @@ const data = document.getElementById('data');
 // 檢查
 // console.log(data);
 
+// 處理localStorage getItem 沒得到值會回傳null
+// 不論得到值或設定值都使用字串值
+// 宣告購物車陣列
+let cart = [];
+
+if (localStorage.getItem('cart')) {
+    // 從localStorage得到cart儲存的值
+    const cartJson = localStorage.getItem('cart');
+
+    // cart轉為js的資料格式(物件陣列)
+    // 前提假設parse不會發生格式錯誤，不然要用try...catch
+    cart = JSON.parse(cartJson);
+
+} else {
+    // 商品資料要轉為json字串再設定進入
+    localStorage.setItem('cart', JSON.stringify(products));
+
+    // 第一次或localStorage中沒有記錄時，使用原始資料
+    cart = products;
+}
+
+
 // 套上tr與td標記在所需值上
-const displayList = products.map((v) => {
+const displayList = cart.map((v) => {
     return `<tr>
                 <td>${v.id}</td>
                 <td><img src="${v.picture}"></td>
