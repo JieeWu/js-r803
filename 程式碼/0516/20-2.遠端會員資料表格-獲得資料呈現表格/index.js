7dceeb03e8b626ab0b24b684570b89b1e5d42433
yisichoco <yisichoco@gmail.com>
2023-05-16 11:59:18 +0800

    Update index.js

--- 程式碼/0516/20-2.遠端會員資料表格-獲得資料呈現表格/index.js
+++ 程式碼/0516/20-2.遠端會員資料表格-獲得資料呈現表格/index.js
@@ -5,25 +5,23 @@ const data = document.getElementById('data');
 // fetch
 const url = 'https://my-json-server.typicode.com/eyesofkids/json-fake-data/users';
 
+function display(users) {
+    const displayList = users.map((v) => {
+        return `<tr>
+                  <td>${v.id}</td>
+                  <td>${v.name}</td>
+                  <td>${v.age}</td>
+                </tr>`;
+    })
+
+    data.innerHTML = displayList.join("");
+}
+
 fetch(url, { method: 'get' })
     //第一次回傳是response物件
-    .then(function (res) {
-        return res.json(); // 要轉為js的物件陣列資料
-    })
-    // 這裡才能得到js的物件陣列
-    .then(function (users) {
-        console.log(users);
+    .then((res) => res.json()) // 要轉為js的物件陣列資料
+    .then(display) // 這裡才能得到js的物件陣列
+    .catch( (error) => console.log(error))
 
-        const displayList = users.map((v) => {
-            return `<tr>
-                        <td>${v.id}</td>
-                        <td>${v.name}</td>
-                        <td>${v.age}</td>
-                    </tr>`
-        })
 
-        data.innerHTML = displayList.join("");
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+
